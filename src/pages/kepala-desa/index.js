69489d238fa8de5f7b2aac4b25edc064dbf6b6e9
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-02-01 17:35:32 +0800

    Add files via upload

--- src/pages/kepala-desa/index.js
+++ src/pages/kepala-desa/index.js
@@ -1,1 +1,85 @@
+import { useEffect, useState } from "react";
+import SekretarisAside from "./sekretarisAside";
+import { db } from "../../../public/firebaseConfig";
+import { getDocs, collection, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
 
+async function fetchDataFromFirestore() {
+    const querySnapshot = await getDocs(collection(db, "surat"));
+
+    const data = [];
+
+    querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+    });
+    return data;
+}
+
+export default function Home() {
+    const [dataSurat, SetDataSurat] = useState([]);
+    const [dataSuratMasuk, SetDataSuratMasuk] = useState([]);
+    const [dataSuratKeluar, SetDataSuratKeluar] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetchDataFromFirestore();
+            SetDataSurat(data);
+
+            // Pisahkan data berdasarkan jenis surat
+            const suratMasuk = data.filter((surat) => surat.jenis_surat === "surat masuk");
+            const suratKeluar = data.filter((surat) => surat.jenis_surat === "surat keluar");
+
+            SetDataSuratMasuk(suratMasuk);
+            SetDataSuratKeluar(suratKeluar);
+        }
+
+        fetchData();
+    }, []);
+    return (
+        <>
+            <div className="sekretaris d-flex">
+                <SekretarisAside />
+                <article style={{ maxHeight: '100vh', overflowY: 'auto' }}>
+                <h1 style={{textAlign: 'center', margin: '20px'}}>Daftar Arsip Surat Masuk dan Keluar</h1>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col" style={{ display: 'none' }}>ID</th>
+                                <th scope="col">No</th>
+                                <th scope="col">File Arsip</th>
+                                <th scope="col">Tanggal Masuk</th>
+                                <th scope="col">Tanggal Keluar</th>
+                                <th scope="col">Nama Penerima</th>
+                                <th scope="col">Alamat Pengirim</th>
+                                <th scope="col">No.Surat</th>
+                                <th scope="col">Jenis Surat</th>
+                                <th scope="col">Tanggal Surat</th>
+                                <th scope="col">Sifat Surat</th>
+                                <th scope="col">Perihal Lampiran</th>
+                                <th scope="col">No WhatsApp</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {dataSurat.map((value, index) => (
+                                <tr key={value.id}>
+                                    <td style={{ display: 'none' }}></td>
+                                    <td>{index + 1}</td>
+                                    <td>{value.file}</td>
+                                    <td>{value.tanggal_masuk}</td>
+                                    <td>{value.tanggal_keluar}</td>
+                                    <td>{value.nama}</td>
+                                    <td>{value.alamat}</td>
+                                    <td>{value.no_surat}</td>
+                                    <td><b>{value.jenis_surat}</b></td>
+                                    <td>{value.tanggal_surat}</td>
+                                    <td>{value.sifat_surat}</td>
+                                    <td>{value.perihal}</td>
+                                    <td>{value.no_wa}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </article>
+            </div>
+        </>
+    )
+}
