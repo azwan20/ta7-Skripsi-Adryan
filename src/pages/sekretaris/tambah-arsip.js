88ddd7ec3675a133d1e032b69001c843dd32dc15
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-02-01 17:04:25 +0800

    Update tambah-arsip.js

--- src/pages/sekretaris/tambah-arsip.js
+++ src/pages/sekretaris/tambah-arsip.js
@@ -149,49 +149,57 @@ export default function TambahArsip() {
                 <SekretarisAside />
                 <article className="d-flex flex-column align-items-center justify-content-between" style={{ maxHeight: '100vh', overflowY: 'auto' }}>
                     <form onSubmit={handleSubmit} method="post" action="">
-                            <section>
-                                <span>
-                                    <p>File Arsip</p>
-                                    <input type="file" name="file" id="nama" value={file} onChange={(e) => setFile(e.target.value)} />
-                                </span>
-                                <span>
-                                    <p>Tanggal Masuk</p>
-                                    <input type="date" name="tanggal_masuk" id="nama" value={tanggal_masuk} onChange={(e) => setTanggal_masuk(e.target.value)} />
-                                </span>
-                                <span>
-                                    <p>Alamat Pengirim</p>
-                                    <input type="text" name="alamat" id="nama" value={alamat} onChange={(e) => setAlamat(e.target.value)} />
-                                </span>
-                                <span>
-                                    <p>No. Surat</p>
-                                    <input type="text" name="no" id="nama" value={no_surat} onChange={(e) => setNo_surat(e.target.value)} />
-                                </span>
-                                <span>
-                                    <p>Jenis Surat</p>
-                                    <input type="text" name="jenis" id="nama" value={jenis_surat} onChange={(e) => setJenis(e.target.value)} />
-                                </span>
-                                <span>
-                                    <p>Tanggal Surat</p>
-                                    <input type="date" name="tanggal_surat" id="nama" value={tanggal_surat} onChange={(e) => setTanggal_surat(e.target.value)} />
-                                </span>
-                                <span>
-                                    <p>Sifat Surat</p>
-                                    <input type="text" name="sifat" id="nama" value={sifat_surat} onChange={(e) => setSifat(e.target.value)} />
-                                </span>
-                                <span>
-                                    <p>Perihal Lampiran</p>
-                                    <input type="text" name="perihal" id="nama" value={prihal} onChange={(e) => setPerihal(e.target.value)} />
-                                </span>
-                                <span>
-                                    <p>No. WhatsApp</p>
-                                    <input type="text" name="no_wa" id="nama" value={no_wa} onChange={(e) => setNo_wa(e.target.value)} />
-                                </span>
-                            </section>
-                            <section className="d-flex justify-content-between">
-                                <button type="submit" onClick={handleSimpanClick} style={{ backgroundColor: '#27323A' }}>Simpan</button>
-                                <button style={{ backgroundColor: '#900000' }}>Batal</button>
-                            </section>
-                        </form>
+                        <section>
+                            <span>
+                                <p>File Arsip</p>
+                                <input type="file" name="file" id="nama" value={file} onChange={(e) => setFile(e.target.value)} />
+                            </span>
+                            <span>
+                                <p>Tanggal Masuk</p>
+                                <input type="date" name="tanggal_masuk" id="nama" value={tanggal_masuk} onChange={(e) => setTanggal_masuk(e.target.value)} />
+                            </span>
+                            <span>
+                                <p>Alamat Pengirim</p>
+                                <input type="text" name="alamat" id="nama" value={alamat} onChange={(e) => setAlamat(e.target.value)} />
+                            </span>
+                            <span>
+                                <p>No. Surat</p>
+                                <input type="text" name="no" id="nama" value={no_surat} onChange={(e) => setNo_surat(e.target.value)} />
+                            </span>
+                            <span>
+                                <p>Jenis Surat</p>
+                                <select
+                                    value={jenis_surat}
+                                    onChange={(e) => setJenis(e.target.value)}
+                                    required  // Tambahkan atribut required di sini
+                                    >
+                                    <option value="" disabled>Pilih Jenis Surat</option>
+                                    <option value="surat masuk">Surat Masuk</option>
+                                    <option value="surat keluar">Surat Keluar</option>
+                                </select>
+                            </span>
+                            <span>
+                                <p>Tanggal Surat</p>
+                                <input type="date" name="tanggal_surat" id="nama" value={tanggal_surat} onChange={(e) => setTanggal_surat(e.target.value)} />
+                            </span>
+                            <span>
+                                <p>Sifat Surat</p>
+                                <input type="text" name="sifat" id="nama" value={sifat_surat} onChange={(e) => setSifat(e.target.value)} />
+                            </span>
+                            <span>
+                                <p>Perihal Lampiran</p>
+                                <input type="text" name="perihal" id="nama" value={prihal} onChange={(e) => setPerihal(e.target.value)} />
+                            </span>
+                            <span>
+                                <p>No. WhatsApp</p>
+                                <input type="text" name="no_wa" id="nama" value={no_wa} onChange={(e) => setNo_wa(e.target.value)} />
+                            </span>
+                        </section>
+                        <section className="d-flex justify-content-between">
+                            <button type="submit" onClick={handleSimpanClick} style={{ backgroundColor: '#27323A' }}>Simpan</button>
+                            <button style={{ backgroundColor: '#900000' }}>Batal</button>
+                        </section>
+                    </form>
                 </article>
             </div>
             {/* Pop-up component */}
