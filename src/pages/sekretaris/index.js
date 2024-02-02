622dd280f548e735368dab0a2d6a7e9c9e6debb7
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-02-02 17:07:53 +0800

    Add files via upload

--- src/pages/sekretaris/index.js
+++ src/pages/sekretaris/index.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
-import SekretarisAside from "./sekretarisAside";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { db } from "../../../public/firebaseConfig";
-import { getDocs, collection, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
-import Navbar from "./navbar";
+import { getDocs, collection } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
-async function fetchDataFromFirestore() {
-    const querySnapshot = await getDocs(collection(db, "surat"));
+async function fetchDataLoginFromFirestore() {
+    const querySnapshot = await getDocs(collection(db, "login"));
 
     const data = [];
 
@@ -15,76 +15,118 @@ async function fetchDataFromFirestore() {
     return data;
 }
 
-export default function Home() {
-    const [dataSurat, SetDataSurat] = useState([]);
-    const [dataSuratMasuk, SetDataSuratMasuk] = useState([]);
-    const [dataSuratKeluar, SetDataSuratKeluar] = useState([]);
+async function fetchDataLoginSekretaris() {
+    const querySnapshot = await getDocs(collection(db, "loginSekretaris"));
+
+    const dataSekretaris = [];
+
+    querySnapshot.forEach((doc) => {
+        dataSekretaris.push({ id: doc.id, ...doc.data() });
+    });
+    return dataSekretaris;
+}
+
+async function fetchDataLoginKepalaDesa() {
+    const querySnapshot = await getDocs(collection(db, "loginKades"));
+
+    const dataKades = [];
+
+    querySnapshot.forEach((doc) => {
+        dataKades.push({ id: doc.id, ...doc.data() });
+    });
+    return dataKades;
+}
+
+export default function Login() {
+    const [dataLogin, SetDataLogin] = useState([]);
+    const [dataLoginSekretaris, SetDataLoginSekretaris] = useState([]);
+    const [dataLoginKades, SetDataLoginKades] = useState([]);
+    const router = useRouter();
 
     useEffect(() => {
         async function fetchData() {
-            const data = await fetchDataFromFirestore();
-            SetDataSurat(data);
-
-            // Pisahkan data berdasarkan jenis surat
-            const suratMasuk = data.filter((surat) => surat.jenis_surat === "surat masuk");
-            const suratKeluar = data.filter((surat) => surat.jenis_surat === "surat keluar");
-
-            SetDataSuratMasuk(suratMasuk);
-            SetDataSuratKeluar(suratKeluar);
+            const data = await fetchDataLoginFromFirestore();
+            SetDataLogin(data);
         }
-
         fetchData();
     }, []);
+
+    useEffect(() => {
+        async function fetchData() {
+            const dataSekretaris = await fetchDataLoginSekretaris();
+            SetDataLoginSekretaris(dataSekretaris);
+        }
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        async function fetchData() {
+            const dataKades = await fetchDataLoginKepalaDesa();
+            SetDataLoginKades(dataKades);
+        }
+        fetchData();
+    }, []);
+
+    const [emailInput, setEmailInput] = useState("");
+    const [passwordInput, setPasswordInput] = useState("");
+
+    const handleLogin = () => {
+        // Memeriksa apakah nilai input sama dengan salah satu email di dataLogin
+        const isLoginSuccessful = dataLogin.some((user) => user.email === emailInput && user.password === passwordInput);
+        // const loginKades = dataLoginKades.some((user) => user.email === emailInput && user.password === passwordInput);
+        const loginSekretaris = dataLoginSekretaris.some((user) => user.email === emailInput && user.password === passwordInput);
+        const kades = emailInput === "kades1" && passwordInput === "1234";
+
+        if (emailInput == "sekretaris@gmail.com" && passwordInput == "1234567") {
+            alert("Login berhasil");
+            router.push('/sekretaris/home');
+        } else if (emailInput == "kades@gmail.com" && passwordInput == "123") {
+            alert("Selamat Datang Kepala Desa");
+            router.push('/kepala-desa');
+        } else {
+            alert("Email atau Password Salah!!!");
+        }
+    };
+
+
     return (
         <>
-            <div className="sekretaris d-flex">
-                <SekretarisAside />
-                <article style={{ maxHeight: '100vh', overflowY: 'auto' }}>
-                    <div className="d-flex align-items-center p-2">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/b/bc/Luwu_Utara_Logo_%28North_Luwu%29.png" alt="Profile" width={55} height={70} />
-                        <h1 style={{textAlign: 'center', margin: 'auto'}}>Kantor Desa Pao</h1>
+            <div className="login d-flex flex-column">
+                <div className="d-flex " style={{ height: "20%" }}>
+                    <img src="/logo-umi.svg" alt="logo-umi" />
+                </div>
+                <div
+                    className="cards d-flex justify-content-center align-items-center"
+                    style={{ height: "80%" }}
+                >
+                    <div className="card justify-content-between align-items-center">
+                        <span>
+                            <h3>Login</h3>
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                onChange={(e) => setEmailInput(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                onChange={(e) => setPasswordInput(e.target.value)}
+                            />
+                            {/* <Link href="#">Forget Password</Link> */}
+                        </span>
+                        <span>
+                            {/* <button style={{ color: '#000' }} >Login With Google</button> */}
+                            <button
+                                style={{ backgroundColor: "#239843" }}
+                                onClick={handleLogin}
+                            >
+                                Login
+                            </button>
+                            {/* <p style={{ textAlign: 'center' }}>Don't have an account? <Link href="#">Register</Link></p> */}
+                        </span>
                     </div>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col" style={{ display: 'none' }}>ID</th>
-                                <th scope="col">No</th>
-                                <th scope="col">File Arsip</th>
-                                <th scope="col">Tanggal Masuk</th>
-                                <th scope="col">Tanggal Keluar</th>
-                                <th scope="col">Nama Penerima</th>
-                                <th scope="col">Alamat Pengirim</th>
-                                <th scope="col">No.Surat</th>
-                                <th scope="col">Jenis Surat</th>
-                                <th scope="col">Tanggal Surat</th>
-                                <th scope="col">Sifat Surat</th>
-                                <th scope="col">Perihal Lampiran</th>
-                                <th scope="col">No WhatsApp</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {dataSurat.map((value, index) => (
-                                <tr key={value.id}>
-                                    <td style={{ display: 'none' }}></td>
-                                    <td>{index + 1}</td>
-                                    <td>{value.file}</td>
-                                    <td>{value.tanggal_masuk}</td>
-                                    <td>{value.tanggal_keluar}</td>
-                                    <td>{value.nama}</td>
-                                    <td>{value.alamat}</td>
-                                    <td>{value.no_surat}</td>
-                                    <td><b>{value.jenis_surat}</b></td>
-                                    <td>{value.tanggal_surat}</td>
-                                    <td>{value.sifat_surat}</td>
-                                    <td>{value.perihal}</td>
-                                    <td>{value.no_wa}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </article>
-                <Navbar />
+                </div>
             </div>
         </>
-    )
+    );
 }
